41108e4aa375d0eb12eef5937f4874e99e421705
zhiqianZ <63725891+zhiqianZ@users.noreply.github.com>
2025-03-20 22:49:38 -0700

    Add files via upload

---
+++ utils/logPF_normalization.py
@@ -0,0 +1,44 @@
+import numpy as np
+import scipy as sp
+import scanpy as sc
+import anndata as ad
+import time
+
+def do_pf(mtx, sf = None):
+    pf = mtx.sum(axis=1).A.ravel()
+    if not sf:
+        sf = pf.mean()
+    pf = sp.sparse.diags(sf/pf) @ mtx
+    return pf
+
+def norm_pf(mtx):
+    return do_pf(mtx)
+
+def norm_log_pf(mtx):
+    pf_log = np.log1p(do_pf(mtx))
+    return pf_log
+
+def norm_pf_log_pf(mtx):
+    pf_log_pf = do_pf(np.log1p(do_pf(mtx)))
+    return pf_log_pf
+   
+def logPF(counts):
+  norm = norm_log_pf(sp.sparse.csr_matrix(counts))
+  return norm.T
+ 
+def PFlogPF(counts):
+  norm = norm_pf_log_pf(sp.sparse.csr_matrix(counts))
+  return norm.T
+  
+def Scanpy(data):
+  adata = sc.AnnData(X=data)
+  sc.pp.normalize_total(adata)
+  sc.pp.log1p(adata)
+  return adata.X.T
+
+  
+  
+  
+  
+  
+  
