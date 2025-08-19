615b474fdc60f4820e1f3262e3589a5476c01763
Mark Robinson <mark.robinson@mls.uzh.ch>
2025-08-19 10:44:23 +0200

    fix merge errors

--- install_packages.R
+++ install_packages.R
@@ -1,30 +1,19 @@
 options(repos="https://stat.ethz.ch/CRAN",
-<<<<<<< HEAD
 	BioC_mirror="https://ftp.gwdg.de/pub/misc/bioconductor")
-=======
-        BioC_mirror="https://ftp.gwdg.de/pub/misc/bioconductor")
 
 # notes: 
 # - currently running it on Ubuntu 22.04
-# - needs cmake >= 3.24
->>>>>>> 515a61481b12a0382559e6527091259d7318410e
+# - will need cmake >= 3.24
+# - not installing Rliger yet
 
 install.packages("BiocManager")
 
 pkgs <- c("scDesign3","SingleCellExperiment","readr","rmarkdown",
-<<<<<<< HEAD
 	  "Seurat","dplyr","GEOquery","R.utils", "here", "markdown",
-=======
-          "Seurat","dplyr","GEOquery","R.utils", "here", "markdown",
->>>>>>> 515a61481b12a0382559e6527091259d7318410e
-          "remotes","distances","harmony","rliger")
+          "remotes","distances","harmony","Rfast")
 
 BiocManager::install(pkgs, Ncpus=10)
 
 BiocManager::install("miraisolutions/godmode", Ncpus=10)
 BiocManager::install("satijalab/seurat-wrappers", Ncpus=10)
-<<<<<<< HEAD
 
-
-=======
->>>>>>> 515a61481b12a0382559e6527091259d7318410e
